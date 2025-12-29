3b8fe434af76127f8d09c06932c15a5ad97881c3
Piyush Prajapati <piyushprajapati7120@gmail.com>
2025-12-29 18:01:41 +0530

    Adding Loaders and Setting strict Auth for SignUP and Login

--- frontend/src/pages/Authentication.jsx
+++ frontend/src/pages/Authentication.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { AuthContext } from '../contexts/AuthContext';
 import { Lock, User, CheckCircle, XCircle } from 'lucide-react';
 import { FcGoogle } from "react-icons/fc";
-import Loader from './Loader'; // Ensure this path is correct
+import Loader from '../components/Loader'; // Ensure this path is correct
 
 export default function Authentication() {
     const [username, setUsername] = React.useState("");
